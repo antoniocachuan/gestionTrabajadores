839ece6ebe4ace2ad2c402a16ce8fbf1b018c543
Antonio Cachuan <alipazaga07@gmail.com>
2014-02-27 23:33:13 -0500

    Avanzando el login falta implementación del service

---
+++ src/java/pe/gestortrabajadores/controller/LoginController.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.gestortrabajadores.controller;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import org.primefaces.context.RequestContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author AntOniO
+ */
+@Component
+@ManagedBean
+@SessionScoped
+@ComponentScan("pe.gestortrabajadores.service")
+public class LoginController {
+
+    public LoginController() {
+    }
+    
+    private String username;  
+      
+    private String password;  
+      
+    public String getUsername() {  
+        return username;  
+    }  
+  
+    public void setUsername(String username) {  
+        this.username = username;  
+    }  
+  
+    public String getPassword() {  
+        return password;  
+    }  
+  
+    public void setPassword(String password) {  
+        this.password = password;  
+    }  
+  
+    public void login(ActionEvent actionEvent) {  
+        RequestContext context = RequestContext.getCurrentInstance();  
+        FacesMessage msg = null;  
+        boolean loggedIn = false;  
+          
+        if(username != null  && username.equals("admin") && password != null  && password.equals("admin")) {  
+            loggedIn = true;  
+            msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);  
+        } else {  
+            loggedIn = false;  
+            msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Login Error", "Invalid credentials");  
+        }  
+          
+        FacesContext.getCurrentInstance().addMessage(null, msg);  
+        context.addCallbackParam("loggedIn", loggedIn);  
+    }  
+    
+}
